ae61895b7e1a720a7b541a09e183234531dd85aa
sun <sleepsun123@163.com>
2020-09-26 00:31:23 +0800

    add index path return 403

--- router/router.go
+++ router/router.go
@@ -1,6 +1,9 @@
 package router
 
 import (
+	"KodiLive/controller"
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -9,7 +12,15 @@ func InitRouter() *fiber.App {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendStatus(403)
+	})
+
+	app.Get("/index.m3u", func(c *fiber.Ctx) error {
+		x, err := controller.GetIndex(c.BaseURL())
+		if err != nil {
+			fmt.Println(err)
+		}
+		return c.SendString(x)
 	})
 
 	return app
